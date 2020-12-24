1eaa20213413ef15ffa5a1fd925d81c1497e0daa
xlnl <lamanhnle@gmail.com>
2020-12-23 20:54:44 -0500

    added in other half of models, planning on merging to main branch soon for review

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,6 +6,11 @@ db.mongoose = mongoose
 
 db.user = require('./user.model')
 db.role = require('./role.model')
+db.comment = require('./comment.model')
+db.post = require('./post.model')
+db.pet = require('./pet.model')
+db.journalEntry = require('./journalEntry.model')
+db.milestone = require('./milestone.model')
 
 db.Roles = ['users', 'admin']
 
