51b0571c67383381435e5299b7866b91e5239aec
granddivino <granddivino@gmail.com>
2020-12-29 18:21:47 -0700

    Updated pet routes

--- backend/routes/pet.routes.js
+++ backend/routes/pet.routes.js
@@ -1,23 +1,45 @@
-// gabe
-// renders pet profile & stretch goal components
-// CRUD pets
+const user = require("../controllers/post.controller")
+const pet = require("../controllers/pet.controller")
+const journalEntry = require("../controllers/journalEntry.controller")
+const milestone = require("../controllers/milestone.controller")
+let router = require("express").Router();
+
 
 // do pets first!! stretch goals later
 // /:username/:pet_id
 
 module.exports = app => {
-    const pet = require('../controllers/pet.controller.js')
-    let router = require('express').Router()
 
-    //Render pet profile specific to user
-    router.get('/:username/:pet_id', pet.findOne)
+    // get user's pet's profile to render journal entries
+    router.get("/:pet_id", pet.findAllJournals, pet.findAllMilestones, pet.findOne)
+
     //Form to add pet a pet to user profile
-    router.post('/:username/:pet_id', pet.create)
+    router.post('/pet', pet.create)
+    
     //Update a pet with id
-    router.put('/:username/:pet_id', pet.update)
-    //Delete a pet
-    router.delete('/:username/:pet_id', pet.delete)
-  
+    router.put('/:pet_id', pet.update)
 
-    app.use('/:username/:pet_id', router)
+    //Delete a pet
+    router.delete('/:pet_id', pet.delete)
+
+    // post route for user's pet's journal entry
+    router.post("/:pet_id/journal", journalEntry.create)
+
+    // update route for user's pet's journal entry
+    router.update("/:pet_id/:journal_id", journalEntry.update)
+    
+    // delete route for user's pet's journal entry
+    router.delete("/:pet_id/:journal_id", journalEntry.delete)
+
+    // post route for user's pet's milestone
+    router.post("/:pet_id/milestone", milestone.create)
+
+    // update route for user's pet's milestone
+    router.update("/:pet_id/:milestone_id", milestone.update)
+    
+    // delete route for user's pet's milestone
+    router.delete("/:pet_id/:milestone_id", milestone.delete)
+
+
+    app.use('/:username', router)
 }
