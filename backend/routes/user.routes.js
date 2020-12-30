a6437424d7acc57204b437460bd313365f097ae1
NatrezC <cnatrez@gmail.com>
2020-12-29 19:09:17 -0600

    updated post.controller

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -18,7 +18,14 @@ module.exports = function(app) {
     app.get("/api/test/admin", [authJwt.verifyWebToken, authJwt.isAdmin],
     controller.adminBoard
     )
-
+    //user goes to own page
     app.get('/:username', [authJwt.verifyWebToken], controller.userBoard)
-
+    //user is grabbing the specific post
+    app.get('/:username/:post_id', [authJwt.verifyWebToken], posts.findOne)
+    //renders form to create a post
+    app.post('/:username/post', [authJwt.verifyWebToken], posts.create)
+    //update post
+    app.put('/:username/:post_id', [authJwt.verifyWebToken], posts.update)
+    //delete
+    app.delete('/:username/:post_id', [authJwt.verifyWebToken], posts.delete)
 }    
