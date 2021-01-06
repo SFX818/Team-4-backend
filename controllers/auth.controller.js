2264e9da6a90f4e1a281fd6b97b6c23b6bf88aa0
granddivino <granddivino@gmail.com>
2021-01-05 18:35:29 -0700

    Updated auth contoller with team

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,29 +4,35 @@ const db = require('../models/index')
 const User = db.user
 const Role = db.role
 
-//This will give us access to encodeand decode the jwt itself (allows us to workwith jwt)
+//This will give us access to encode and decode the jwt itself (allows us to workwith jwt)
 const jwt = require('jsonwebtoken')
 //For hashing / encrypting out passwords
 const bcrypt = require('bcryptjs')
 
 //This will handle stand up
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
     
     //We are going to make out user object using the params returned from req
+    const password = await req.body.password
+
     const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
         username: req.body.username,
         email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8)
+        city: req.body.city,
+        profilePic: req.body.profilePic,
+        password: bcrypt.hashSync(password, 8),
     })
     // We save that user, and if there is an error, we throw that error
-    user.save((err, user) => {
+    await user.save((err, user) => {
         if (err) {
             res.status(500).send({message: err})
             return
         }
         //If no error, we check if roles was passed on req.params
         if(req.body.roles) {
-            Role.find({
+            await Role.find({
                 name: {$in: req.body.roles}
             }, (err, roles) => {
                 if (err) {
@@ -49,7 +55,7 @@ exports.signup = (req, res) => {
             })
 
         } else {
-            Role.findOne({name: 'user'}, (err, role) => {
+            await Role.findOne({name: 'user'}, (err, role) => {
                 if(err) {
                     res.status(500).send({message: err})
                     return
@@ -68,6 +74,8 @@ exports.signup = (req, res) => {
             })
         }
 
+    }).catch(err => {
+        console.log(err)
     })
 }
 
@@ -112,6 +120,8 @@ exports.signin = (req, res) => {
             id: user._id,
             username: user.username,
             email: user.email,
+            city: user.city,
+            profilePic: user.profilePic,
             roles: authorities,
             accessToken: token
         })
