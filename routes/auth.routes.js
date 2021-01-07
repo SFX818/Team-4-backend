9b4f3cdd83ad8ad57d56ba1e2e92dd0228579feb
NatrezC <cnatrez@gmail.com>
2021-01-06 20:21:57 -0600

    set header to use x access token

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,2 +1,12 @@
 const { verifySignup } = require('../middlewares/')
 const controller = require('../controllers/auth.controller')
+
+module.exports = function (app) {
+    app.use((req, res, next) => {
+        // set header and allow use of x access token ( we will use this to pass our token )
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-type, Accept"
+        );
+        next();
+    })
